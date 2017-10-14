bd229dacb72372aa4f400a53ef5aa92eec3db17f
Unknown <weichuan.tran@gmail.com>
2017-10-14 14:31:40 +0200

    up

--- exercice 3/script.js
+++ exercice 3/script.js
@@ -1,7 +1,7 @@
-var soustraction = 3 - 4 ;
-var division = 2 / 4;
-var multiplication = 3 * 2;
-var pourcentage = (50% de 10);
-var vitesse = ("distance, heure");
 
-result = ""
+function additionner(a, b) {
+  var a = 5;
+  var b = 3;
+  result =(a - b)
+
+}
