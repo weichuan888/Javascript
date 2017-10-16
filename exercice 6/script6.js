bcb7ea6e1290624721a4deeaa5ea2114403d398a
Unknown <weichuan.tran@gmail.com>
2017-10-16 15:15:41 +0200

    update

---
+++ exercice 6/script6.js
@@ -0,0 +1,12 @@
+
+
+var table = window.prompt ("Le pendu");
+
+
+var tableau = ["B", "O", "N", "J", "O", "U", "R"];
+var pendu = ['',"",'',"",'';"";'_'];
+var fin = tableau.lenght;
+
+for (var i=0; i <tableau.length; i++) {
+  
+}
