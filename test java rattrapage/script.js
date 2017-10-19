21c4857f3b004f60c7b271f177b32bd9fd565263
Unknown <weichuan.tran@gmail.com>
2017-10-19 09:17:22 +0200

    up

---
+++ test java rattrapage/script.js
@@ -0,0 +1,40 @@
+// // exercice condition
+// const t = 5;
+//
+// function mathMax(a, b) {
+//   let max = 0;
+//   if (a>b) {
+//     max = a;
+//   }
+//   else {
+//     max = b;
+//   }
+//   return max;
+// }
+//
+//
+// const c = 2000
+// function salaire(c) {
+//     if (c>1200) {
+//       console.log(1200+((c/100)*15)) }
+//     else {
+//       console.log ("pas de retenu")}}
+// salaire()
+//
+//
+// const brut = 2000
+// const net = 1200
+// function Salai(brut, net) {
+//   if ()
+
+var a=45;
+var c=78;
+
+//cree une fonction qui va multiplier a et c qui crée un resultat qui va donner x
+
+function(){
+  var a=0;
+}
+
+
+console.log(x)
