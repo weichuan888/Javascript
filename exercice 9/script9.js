f36d1345dfc8bb1385b7d823c9999e9c905fda76
Unknown <weichuan.tran@gmail.com>
2017-10-23 00:55:26 +0200

    update

---
+++ exercice 9/script9.js
@@ -0,0 +1,10 @@
+var juste_prix = prompt ("Jeu: le juste prix");
+var essai = 0;
+var min = Math.min(20);
+var max = Math.max(80);
+
+function aleatoire () {
+    for (var i = 0; i<(x,y); i++);
+    x = (Math.floor(Math.random()*Math.min));
+    y = (Math.floor(Math.random()*Math.max));
+}
--- test java rattrapage/script.js
+++ test java rattrapage/script.js
@@ -76,40 +76,125 @@ else  {
 guessLetter()
 */
 
-alert("Le jeu du pendu, enjoy!");
+// alert("Le jeu du pendu, enjoy!");
+//
+// var tableau = ["B", "O", "N", "J", "O", "U", "R"];
+// var tableauVide = ["","","","","","",""];
+// var fin = tableau.length; /* => 7*/
+// var cpt = 0;
+//
+// /* Cree une fonction guessLetter */
+// while( fin > 0){
+//
+//     var input = prompt ("Choisis ta lettre");
+//     input=input.toUppercase();
+//
+//     if(tableau.includes(input)){
+// /* Iterer a travers les lettres */
+//     for (var i=0; i<tableau.length; i++) {
+// /* Voir si la lettre deviné se trouve dans le mot */
+//         if (tableau[i] == input) {
+//           var index=tableau.indexOf(input);
+//           tableauVide.slice(index, 1, input);
+//           tableau.slice(index, 1, "*");
+//           fin--;
+//           console.log(tableauVide)
+//           }
+//       }
+//     }
+//
+//       else {
+//         cpt++;
+//         console.log("Erreur, recommence")
+//
+//       }
+//
+// }
+//
+// console.log("Bravo, vous avez gagné!");
+// console.log(cpt + " erreur(s)!");
 
-var tableau = ["B", "O", "N", "J", "O", "U", "R"];
-var tableauVide = ["","","","","","",""];
-var fin = tableau.length; /* => 7*/
-var cpt = 0;
 
-/* Cree une fonction guessLetter */
-while( fin > 0){
+// exercice 1
+//
+// var nb_enfants = 4;
+// var nom_femme = "Li";
+// var pays = "France";
+// var metier = "pompier";
+//
+// console.log(" Vous serez " + metier + " et habiterez en " + pays + " et marié à " + nom_femme + " avec " + nb_enfants + " enfants.");
+//
 
-    var input = prompt ("Choisis ta lettre");
-    input=input.toUppercase();
+// var annee_courante = 2017;
+// var annee_naissance = 1993;
+//
+// console.log(annee_courante - annee_naissance);
+//
+//
+// var age = 24;
+// var age_max = 100;
+// var denree_alimentaire = "bananes";
+// var par_jour = 2;
+//
+// var vie = par_jour * 365;
+// var age_restant = age_max - age;
+//
+// console.log("Il vous reste " + vie + " de " + denree_alimentaire + " avant d'atteindre l'age de " + age_max + " ans. ");
 
-    if(tableau.includes(input)){
-/* Iterer a travers les lettres */
-    for (var i=0; i<tableau.length; i++) {
-/* Voir si la lettre deviné se trouve dans le mot */
-        if (tableau[i] == input) {
-          var index=tableau.indexOf(input);
-          tableauVide.slice(index, 1, input);
-          tableau.slice(index, 1, "*");
-          fin--;
-          console.log(tableauVide)
-          }
-      }
-    }
+//
+// var etape1 = 1+2;
+// var etape2= etape1*3;
+// var etape3 = 4/2;
+// var etape4 = etape2 + etape3;
+//
+// console.log("etape 1 : " + etape1 + "* 3 + 4/2");
+// console.log("etape 2 : " + etape2 + " + 4/2" );
+// console.log("etape 3 : " + etape2 + " + " + etape3);
+// console.log("etape 4 : " + etape4);
 
-      else {
-        cpt++;
-        console.log("Erreur, recommence")
+// Exerice 2
+//
+// var langue1 = "fr",
+//  langue2 = "es",
+//  langue3 = "en";
+//
+// var msg = prompt ("Quel langue parles-tu?");
+// if (msg == "fr") {
+//   console.log("Bonjour tout le monde");
+// }
+// if (msg == "es") {
+//   console.log("Hola, Mundo");
+// }
+// if (msg == "en") {
+//   console.log("Hello World");
+// }
 
-      }
+// var score = prompt ("ton score ?");
+// var result = "";
+// if (score >=90) {
+// result = "rang A"
+// }
+// if (score < 90) {
+// result = "rang B"
+// }
+// if (score <=50) {
+// result = "Rang C"
+// }
+// console.log(result);
 
+
+
+// var singulier = "chocolat";
+// var nombre = 50;
+// var result = "";
+// if (nombre>1) {
+//   var result = singulier + "s";
+// }
+// else {
+//   var result = singulier;
+// }
+// console.log("Je possède 4 " + result  );
+
+var add = function (a, b) {
+  return  a + b
 }
-
-console.log("Bravo, vous avez gagné!");
-console.log(cpt + " erreur(s)!");
