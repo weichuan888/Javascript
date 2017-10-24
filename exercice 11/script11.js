f6b1a6fbe3e1729e4d9c80534a181d64bc736d6c
Unknown <weichuan.tran@gmail.com>
2017-10-24 11:54:59 +0200

    update

---
+++ exercice 11/script11.js
@@ -0,0 +1,4 @@
+var request = new XMLHtppRequest();
+request.open ("GET", "https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json", true);
+request.send ()
+request = null
